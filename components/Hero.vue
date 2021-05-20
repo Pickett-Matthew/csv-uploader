75a0405d7d6fc2f5467b54edba2a09e5601d0060
Christopher Mitchell <cmitchell@crishell.co>
2021-05-20 10:52:31 -0400

    Initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="p-12 flex flex-col items-center border rounded-xl shadow bg-white"
+  >
+    <logo class="mb-16" />
+    <h1 class="text-4xl text-bold mb-8">RMS AppDev Interview Challenge</h1>
+    <h2 class="text-2xl text-bold">Good Luck! 💪</h2>
+  </div>
+</template>
+
+<script>
+import Logo from './Logo'
+
+export default {
+  components: { Logo },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      flex
+      h-screen
+      w-screen
+      items-center
+      justify-center
+      text-gray-700
+      bg-gray-100
+    "
+  >
+    <hero />
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero'
+
+export default {
+  components: { Hero },
+}
+</script>
